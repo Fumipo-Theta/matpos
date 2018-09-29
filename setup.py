9a4f85fae5089411234c0a87c0621ada96ee9a1b
twined quartz <fmorisato@gmail.com>
2018-09-29 17:32:40 +0900

    modify dependency

--- setup.py
+++ setup.py
@@ -21,7 +21,7 @@ setup(
     url='https://github.com/Fumipo-Theta/matpos',
     install_requires=['numpy', 'matplotlib', 'func_helper'],
     dependency_links=[
-        'git+ssh://git@github.com/Fumipo-Theta/func_helper.git@master#egg=func_helper-1.0.0'],
+        'git+ssh://git@github.com/Fumipo-Theta/func_helper.git@master#egg=func_helper'],
     license=license,
     packages=find_packages(exclude=('tests', 'docs'))
 )
